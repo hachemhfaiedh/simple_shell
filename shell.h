f9a8748ddd709be54882c6b416e7d9ad6d9dff7e
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-04-14 23:25:36 +0000

    fix

--- shell.h
+++ shell.h
@@ -1,5 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
+
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,22 +10,13 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <errno.h>
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 4971220526ae6667caa73f0a545636b6dceb92fa
 int _putchar(char c);
 int _print(char *str);
 int main(void);
 int read_line(void);
-<<<<<<< HEAD
-char** parse_line(char *);
-
-#endif
-=======
 char **parse_line(char *);
 extern char **environ;
-int print_env(char **environ);
+char find_env(char **glovar);
+
 #endif
->>>>>>> 4971220526ae6667caa73f0a545636b6dceb92fa
