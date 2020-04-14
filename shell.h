f9c2e4773e55bd52359c1aa01816b04c474a60ae
Immaannn2222 <imennaayari@gmail.com>
2020-04-15 00:46:11 +0100

    fix

--- shell.h
+++ shell.h
@@ -9,22 +9,11 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <errno.h>
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 4971220526ae6667caa73f0a545636b6dceb92fa
 int _putchar(char c);
 int _print(char *str);
 int main(void);
-int read_line(void);
-<<<<<<< HEAD
-char** parse_line(char *);
-
-#endif
-=======
+char *read_line(void);
 char **parse_line(char *);
 extern char **environ;
-int print_env(char **environ);
+char *find_env(char *glovar);
 #endif
->>>>>>> 4971220526ae6667caa73f0a545636b6dceb92fa
