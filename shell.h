be73df44465affcb9329990d4517f7ccbb4ae48f
Immaannn2222 <imennaayari@gmail.com>
2020-04-15 00:07:18 +0100

    fix

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <errno.h>
+
+
+int _putchar(char c);
+int _print(char *str);
+int main(void);
+int read_line(void);
+char** parse_line(char *);
+
+#endif
